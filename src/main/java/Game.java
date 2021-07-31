bf2e1f2ef3ea911a2f84ecc7b69450d809472ad1
Zim Sakib <ximsakib@gmail.com>
2021-08-01 01:31:53 +0600

    fixes some bugs

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -624,8 +624,8 @@ if (b3 == ("X") && b6 == ("X") && b9 == ("X")){
     }//GEN-LAST:event_textExitActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-          Menu menu = new Menu();
-           menu.show();
+//          Menu menu = new Menu();
+    //       menu.show();
            dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
